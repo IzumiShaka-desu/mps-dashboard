73b73b9142e2385c3aff662dc6f1d8ca7d5ca565
IzumiShaka-desu <shakaaji29@gmail.com>
2023-01-23 09:13:04 +0700

    add slider in data table

--- src/components/DefaultTable.jsx
+++ src/components/DefaultTable.jsx
@@ -2,18 +2,68 @@
 //
 import React from "react";
 import DataTable from "react-data-table-component";
+import Slider from '@mui/material/Slider';
+import Box from '@mui/material/Box';
 
-const DefaultTable = ({ data, columns, title }) => {
+const DefaultTable = ({ data, columns, title, footer }) => {
+
+    const conditionalRowStyles = [
+        {
+            when: row => row.type.toLowerCase().includes("plan"),
+            style: {
+                backgroundColor: "#30C5FF",
+                userSelect: "none",
+                color: "white",
+            }
+        }
+    ];
+    const handleScroll = (state) => {
+        console.log(state);
+    }
+    const [rangeFilter, setRangeFilter] = React.useState([1, columns.length - 2]);
+
+    const handleSliderChange = (event, newValue) => {
+        setRangeFilter(newValue);
+        console.log(newValue);
+    };
+
+
+
     return (
-        <DataTable
-            title={title}
-            columns={columns}
-            data={data}
-            pagination
-            highlightOnHover
-            pointerOnHover
-            responsive
-        />
+        <div onScroll={handleScroll}>
+            <Box sx={{ width: 300 }}>
+
+                <Slider
+                    size="small"
+                    min={1}
+                    value={rangeFilter}
+                    max={columns.length - 2}
+                    aria-label="Small"
+                    valueLabelDisplay="auto"
+                    onChange={handleSliderChange}
+                />
+            </Box>
+            <DataTable
+                title={title}
+                columns={columns.map((column, index) => {
+                    if ((index >= rangeFilter[0] && index <= rangeFilter[1]) || (index == 0 || index == columns.length - 1)) {
+                        return column;
+                    }
+
+                    return null;
+                }).filter((column) => column != null)
+                }
+                data={data}
+                footer={footer}
+                striped={true}
+                pagination
+                paginationPerPage={100}
+                highlightOnHover
+                conditionalRowStyles={conditionalRowStyles}
+                pointerOnHover
+                responsive
+            />
+        </div>
     );
 }
 
