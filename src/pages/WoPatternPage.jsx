0adae02ff9c1bc816e0afc3c0554221b31d96c4d
IzumiShaka-desu <shakaaji29@gmail.com>
2023-01-25 13:52:31 +0700

    add new row (grand total)

--- src/pages/WoPatternPage.jsx
+++ src/pages/WoPatternPage.jsx
@@ -151,10 +151,14 @@ const WoPatternPage = () => {
                     //     let row = [];
                     //     return row;
                     // });
-                    let bottomRow = {
+                    let bottomRow = [{
                         type: "Plan",
+                    }, {
+                        type: "Grand Total",
                     }
+                    ]
                     let rows = getRowsArray(item, dateOfThisMonth);
+                    let totalPlan = 0;
                     dateOfThisMonth.map((date) => {
                         let timeInMs = date.valueOf();
                         // Mengubah milidetik
@@ -165,12 +169,26 @@ const WoPatternPage = () => {
                             return item[fixedDateStr] !== "-";
                         });
                         // assign count of dataByDate to bottomRow[fixedDateStr]
-                        bottomRow[fixedDateStr] = dataByDate.length;
+                        bottomRow[0][fixedDateStr] = dataByDate.length;
+                        bottomRow[1][fixedDateStr] = dataByDate.map((item) => {
+
+                            return parseInt(item[fixedDateStr]);
+                        }).reduce((a, b) => a + b, 0);
+                        console.log(`databydate ${fixedDateStr}`);
+                        console.log(dataByDate);
+
+                        totalPlan += dataByDate.length;
 
                         return 0;
 
                     });
-                    bottomRow["total"] = rows.map((item) => { return item.total }).reduce((a, b) => a + b, 0);
+                    bottomRow[0]["total"] = totalPlan;
+                    bottomRow[1]["total"] =
+                        // rows.map((item) => {
+                        //     return item.total;
+                        // }).reduce((a, b) => a + b, 0);
+
+                        rows.map((item) => { return item.total }).reduce((a, b) => a + b, 0);
 
                     console.log(rows);
 
